5c5e2d04824f0c791181ac77b291acabc879e986
Anthony Brummett <abrummet@genome.wustl.edu>
2012-04-26 16:06:36 -0500

    Do some resetting of the perl process before running the test code
    Some tests expect to run in package 'main'
    
    Some tests look at @ARGV for interactive testing, clear it out first

--- lib/App/Prove/Plugin/Distributed.pm
+++ lib/App/Prove/Plugin/Distributed.pm
@@ -186,7 +186,11 @@ sub _do {
     local *CORE::GLOBAL::exit = sub { die 'notr3a11yeXit' };
     local $0 = $job_info;    #fixes FindBin (in English $0 means $PROGRAM_NAME)
     no strict;               # default for Perl5
-    do $0;                   # do $0; could be enough for strict scripts
+    {
+        package main;
+        local @ARGV = ();
+        do $0;                   # do $0; could be enough for strict scripts
+    }
     if ($EVAL_ERROR) {
         $EVAL_ERROR =~ s{\n+\z}{};
         unless ( $EVAL_ERROR =~ m{^notr3a11yeXit} ) {
